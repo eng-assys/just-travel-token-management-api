1144780a7203fc8a60bc5d59a9ffc6c90c59befb
Lucas Assis <lukas.ecomp@gmail.com>
2026-01-06 10:51:22 -0300

    add dabase url to configuration file

--- config/configuration.ts
+++ config/configuration.ts
@@ -3,6 +3,7 @@ export default () => ({
     port: parseInt(process.env.PORT || '3000', 10),
   },
   database: {
+    url: process.env.DATABASE_URL,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     name: process.env.DB_DATABASE_NAME,
