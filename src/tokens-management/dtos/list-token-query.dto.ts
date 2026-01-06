76e231f053a5cca72cb9b474192ee287c8622385
Lucas Assis <lukas.ecomp@gmail.com>
2026-01-06 14:36:44 -0300

    add ListTokenQueryDto

---
+++ src/tokens-management/dtos/list-token-query.dto.ts
@@ -0,0 +1,28 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsEnum,
+  IsNumberString,
+  IsOptional,
+} from 'class-validator';
+import { TokenStatus } from 'src/generated/prisma/enums';
+
+export class ListTokenQueryDto {
+  @ApiPropertyOptional({
+    description: 'Status of tokens to be shown',
+    enum: TokenStatus,
+    example: TokenStatus.AVAILABLE,
+  })
+  @IsOptional()
+  @IsEnum(TokenStatus)
+  status: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNumberString()
+  page: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNumberString()
+  limit: string;
+}
