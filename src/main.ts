df355f0d0e8736b430409527fddb1069f915fbd1
Lucas Assis <lukas.ecomp@gmail.com>
2026-01-06 14:12:54 -0300

    enable class validator and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import configuration from 'config/configuration';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,16 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Tokens Management')
+    .setDescription('Tokens Management API description')
+    .setVersion('1.0')
+    .addTag('tokens-management')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
 
   await app.listen(configuration().api.port ?? 3000);
 }
