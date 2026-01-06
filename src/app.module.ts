cc1b0aaccb450b45ecda9cb9f22fe261367281d8
Lucas Assis <lukas.ecomp@gmail.com>
2026-01-06 16:57:44 -0300

    add Scheduler module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TokensManagementModule } from './tokens-management/tokens-management.module';
 import configuration from 'config/configuration';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import configuration from 'config/configuration';
       load: [configuration],
       isGlobal: true,
     }),
+    ScheduleModule.forRoot(),
     TokensManagementModule,
   ],
   controllers: [AppController],
