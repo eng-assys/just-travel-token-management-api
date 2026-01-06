8d446b3fe4908563581b389ac149604712a9da62
Lucas Assis <lukas.ecomp@gmail.com>
2026-01-06 14:36:53 -0300

    add ClaimTokenDto

---
+++ src/tokens-management/dtos/claim-token.dto.ts
@@ -0,0 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsUUID,
+} from 'class-validator';
+
+export class ClaimTokenDto {
+  @ApiProperty({
+    description: 'UUID of the user claiming the token',
+    example: '2bcfc2a0-5249-4925-af02-94046226529f',
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsUUID()
+  userId?: string;
+}
