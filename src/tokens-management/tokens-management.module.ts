fd5e35b3fd1bd897419c64a4eb2412c029bc3475
Lucas Assis <lukas.ecomp@gmail.com>
2026-01-06 13:36:08 -0300

    add tokens seed running on module startup

--- src/tokens-management/tokens-management.module.ts
+++ src/tokens-management/tokens-management.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { TokensManagementService } from './tokens-management.service';
 import { TokensManagementController } from './tokens-management.controller';
+import { PrismaService } from 'src/prisma.service';
+import configuration from 'config/configuration';
 
 @Module({
   controllers: [TokensManagementController],
-  providers: [TokensManagementService],
+  providers: [TokensManagementService, PrismaService],
 })
-export class TokensManagementModule {}
+export class TokensManagementModule implements OnModuleInit {
+  constructor(
+    private readonly tokensManagementService: TokensManagementService,
+  ) {}
+
+  async onModuleInit() {
+    if (configuration().api.runSeedOnStartup) {
+      await this.tokensManagementService.seed();
+    }
+  }
+}
--- src/tokens-management/tokens-management.service.ts
+++ src/tokens-management/tokens-management.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class TokensManagementService {}
+export class TokensManagementService {
+  constructor(private prisma: PrismaService) {}
+
+  async seed() {
+    try {
+      console.log('🌱 Seeding tokens...');
+      await this.prisma.usageHistory.deleteMany();
+      await this.prisma.token.deleteMany();
+
+      const tokensToCreate = Array.from({ length: 100 }).map(() => ({
+        status: 'AVAILABLE' as const,
+      }));
+
+      const result = await this.prisma.token.createMany({
+        data: tokensToCreate,
+        skipDuplicates: true,
+      });
+
+      console.log(
+        `✅ Success: ${result.count} tokens created in the database.`,
+      );
+    } catch (error) {
+      console.error('❌ Error running seed:', error);
+      process.exit(1);
+    }
+  }
+}
