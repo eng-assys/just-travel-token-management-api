034828ab0475586e3b7975be263a4c634b4ff7ba
Lucas Assis <lukas.ecomp@gmail.com>
2026-01-06 13:38:48 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import configuration from 'config/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(configuration().api.port ?? 3000);
 }
 bootstrap();
