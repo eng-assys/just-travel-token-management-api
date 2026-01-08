9d50d45f0710d804fd861f049596b36f0b59236b
Lucas Assis <lukas.ecomp@gmail.com>
2026-01-08 16:16:44 -0300

    running claim token for created seed tokens

--- src/database/prisma/seeds/seed.module.ts
+++ src/database/prisma/seeds/seed.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { PrismaService } from '../prisma.service';
+import { TokensManagementService } from 'src/tokens-management/tokens-management.service';
 
 @Module({
-  providers: [SeedService, PrismaService],
+  providers: [SeedService, PrismaService, TokensManagementService],
 })
 export class SeedModule {}
--- src/database/prisma/seeds/seed.service.ts
+++ src/database/prisma/seeds/seed.service.ts
@@ -1,31 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
+import { TokensManagementService } from 'src/tokens-management/tokens-management.service';
 
 @Injectable()
 export class SeedService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly tokensManagementService: TokensManagementService,
+  ) {}
 
   async run() {
     try {
-      console.log('🌱 Seeding tokens...');
       await this.prisma.usageHistory.deleteMany();
       await this.prisma.token.deleteMany();
 
-      const tokensToCreate = Array.from({ length: 100 }).map(() => ({
-        status: 'AVAILABLE' as const,
-      }));
+      const currentUserId = '3a2775bb-b388-4595-a1ae-8a5eb6c59605';
 
-      const result = await this.prisma.token.createMany({
-        data: tokensToCreate,
-        skipDuplicates: true,
-      });
+      await this.seedTokens();
 
-      console.log(
-        `✅ Success: ${result.count} tokens created in the database.`,
-      );
+      for (let index = 0; index < 10; index++) {
+        await this.tokensManagementService.claimToken({
+          userId: currentUserId,
+        });
+      }
     } catch (error) {
       console.error('❌ Error running seed:', error);
       process.exit(1);
     }
+  }
+
+  private async seedTokens() {
+    console.log('🌱 Seeding tokens...');
+
+    const tokensToCreate = Array.from({ length: 100 }).map(() => ({
+      status: 'AVAILABLE' as const,
+    }));
+
+    const result = await this.prisma.token.createMany({
+      data: tokensToCreate,
+      skipDuplicates: true,
+    });
+    console.log(`✅ Success: ${result.count} tokens created in the database.`);
   }
 }
