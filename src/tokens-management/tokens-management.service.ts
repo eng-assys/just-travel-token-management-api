5ed0fb8d12c26109cb20824ee293472ce3a6f16d
Lucas Assis <lukas.ecomp@gmail.com>
2026-01-08 15:18:51 -0300

    remove seed method for token management service

--- src/tokens-management/tokens-management.service.ts
+++ src/tokens-management/tokens-management.service.ts
@@ -11,30 +11,6 @@ import { Prisma, Token } from 'src/generated/prisma/browser';
 export class TokensManagementService {
   constructor(private prisma: PrismaService) {}
 
-  async seed() {
-    try {
-      console.log('🌱 Seeding tokens...');
-      await this.prisma.usageHistory.deleteMany();
-      await this.prisma.token.deleteMany();
-
-      const tokensToCreate = Array.from({ length: 100 }).map(() => ({
-        status: 'AVAILABLE' as const,
-      }));
-
-      const result = await this.prisma.token.createMany({
-        data: tokensToCreate,
-        skipDuplicates: true,
-      });
-
-      console.log(
-        `✅ Success: ${result.count} tokens created in the database.`,
-      );
-    } catch (error) {
-      console.error('❌ Error running seed:', error);
-      process.exit(1);
-    }
-  }
-
   async claimToken(body: ClaimTokenDto) {
     return await this.prisma.$transaction(
       async (tx) => {
